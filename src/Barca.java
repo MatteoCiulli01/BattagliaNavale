d62b3bd434fbb0d4b5e02688a5025c2599e93f2c
lai2 <informatica@lai2>
2019-11-06 09:52:09 +0100

    inserito conttrollo errore sull'input

--- src/Barca.java
+++ src/Barca.java
@@ -4,14 +4,26 @@ public class Barca
     private ArrayList<Coordinate> Coordinate;
     private int puntivita;
     private boolean affondata;
-    public Barca(int PV,ArrayList<Coordinate> Coordinate1)
+    public Barca(int PV,ArrayList<Coordinate> Coordinate1)throws Exception
     {
+        if(PV >= 0 && Coordinate1.size() > 0)
+        {
+            throw new Exception("input n1on vaido");
+        }
         this.puntivita = PV;
         this.Coordinate = Coordinate1;
         this.affondata = false;
     }
-    private boolean Colpo(Coordinate colpo)
+    private boolean Colpo(Coordinate colpo)throws Exception
     {
+        if(colpo.getx() < 21 && colpo.getx() < 0 )
+        {
+            throw new Exception("coordinate x non vaide");
+        }
+        if(colpo.gety() < 21 && colpo.gety() < 0 )
+        {
+            throw new Exception("coordinate y non vaide");
+        }
         if (Coordinate.contains(colpo))
         {
            int pos = Coordinate.indexOf(colpo);
--- src/Coordinate.java
+++ src/Coordinate.java
@@ -3,13 +3,28 @@ public class Coordinate
     private int x;
     private int y;
     boolean inserita = false;
-    public Coordinate(int x,int y)
-    {
+    public Coordinate(int x,int y)throws Exception
+    {        if(x < 21 && x < 0 )
+        {
+            throw new Exception("coordinate x non vaide");
+        }
+        if(y < 21 && y < 0 )
+        {
+            throw new Exception("coordinate y non vaide");
+        }
         this.x= x;
         this.y = y;
     }
     void change_state()
     {
         this.inserita = true;
+    }
+    public int getx()
+    {
+        return this.x;
+    }
+    public int gety()
+    {
+        return this.y;
     }
 }
